22b891e8d4f6b5aa0505bafec2f672aef7e0da07
MonteiroCami <monteiro.isabeldossantos@gmail.com>
2024-10-22 19:56:29 -0400

    adicionei a ultima fase que seria caso a pessoa acerte a terneira pergunta, irá para o finalizou oQuiz, tbm adicionei o a tela de caso perca na 3° pergunta, mas preciso estilizar essas telas. Estou subindo o commit da função da t_perg também.

---
+++ t_perg.js
@@ -0,0 +1,18 @@
+// Seleciona todas as opções de resposta
+const options = document.querySelectorAll('.option');
+
+// Função que será chamada ao clicar em uma das opções
+options.forEach(option => {
+    option.addEventListener('click', function() {
+        const isCorrect = option.getAttribute('data-correct') === 'true';
+        
+        // Verifica se a resposta está correta
+        if (isCorrect) {
+            // Redireciona para a página da próxima pergunta
+            window.location.href = 'finalizou.html';
+        } else {
+            // Redireciona para a página de erro
+            window.location.href = 'pontuação2.html';
+        }
+    });
+});
